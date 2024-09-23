60cc3fda57d1d9115d481fdaf1e3f1b89c7b3f6f
gzitei <gustavo.zitei@gmail.com>
2024-09-23 16:14:53 -0300

    fix not found

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import CardContainer from "./CardContainer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+//TODO make app responsive
+
 const prefix = "/blog";
 
 function App() {
@@ -27,14 +29,12 @@ function App() {
             <Header />
             <Content>
               <Routes>
-                <Route path="/" element={<CardContainer />}></Route>
-                <Route path={prefix + "/"} element={<CardContainer />}></Route>
-                <Route
-                  path={prefix + "/article/:id"}
-                  element={<Article />}
-                ></Route>
-                <Route path={prefix + "/tags"} element={<Tags />}></Route>
-                <Route path={prefix + "/about"} element={<About />}></Route>
+                <Route path="/" element={<CardContainer />} />
+                <Route path={prefix + "/"} element={<CardContainer />} />
+                <Route path={prefix + "/article/:id"} element={<Article />} />
+                <Route path={prefix + "/tags"} element={<Tags />} />
+                <Route path={prefix + "/about"} element={<About />} />
+                <Route path="/*" element={<h2>Content not found!</h2>} />
               </Routes>
             </Content>
             <Footer />
